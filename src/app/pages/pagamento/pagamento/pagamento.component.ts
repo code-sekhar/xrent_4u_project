280f045309b65632e93e70f12d6b284c7b5284a1
code-sekhar <75156986+code-sekhar@users.noreply.github.com>
2023-12-11 02:30:53 +0530

    responsive page

--- src/app/pages/pagamento/pagamento/pagamento.component.ts
+++ src/app/pages/pagamento/pagamento/pagamento.component.ts
@@ -12,4 +12,11 @@ clickEventSucess(){
   this.show_sucess = true;
   this.hide_form = false;
 }
+plan_filter_show:boolean = false;
+planFilters(){
+this.plan_filter_show = !this.plan_filter_show;
+}
+filter_poup_close(){
+  this.plan_filter_show = false;
+}
 }
